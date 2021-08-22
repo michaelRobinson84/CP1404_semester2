f33777dce7c42830772a031ec7c9f0b9d9b13720
Mike <michael.robinson1@my.jcu.edu.au>
2021-08-22 18:52:01 +1000

    Refactor password_check program to use functions

--- cp1404practicals/prac_03/password_check.py
+++ cp1404practicals/prac_03/password_check.py
@@ -1,7 +1,21 @@
-str_password = input("Please enter a password of minimum length 8 characters: ")
+def main():
 
-while len(str_password) < 8:
-    print("Password not long enough!")
+    str_password = get_password()
+
+    print_password(str_password)
+
+
+def print_password(str_password):
+    print((len(str_password) * "*"))
+
+
+def get_password():
     str_password = input("Please enter a password of minimum length 8 characters: ")
+    while len(str_password) < 8:
+        print("Password not long enough!")
+        str_password = input("Please enter a password of minimum length 8 characters: ")
+    return str_password
 
-print((len(str_password) * "*"))
+
+main()
+
