c054cc1ce7a9f91b364200437abc6b0f3441a4b4
Mike <michael.robinson1@my.jcu.edu.au>
2021-08-31 14:37:46 +1000

    Add list_exercises.py

---
+++ cp1404practicals/prac_04/list_exercises.py
@@ -0,0 +1,19 @@
+numbers = []
+for number in range(1, 6):
+    number = int(input("Number: "))
+    numbers.append(number)
+
+print("The first number is {}".format(numbers[0]))
+print("The last number is {}".format(numbers[-1]))
+print("The smallest number is {}".format(min(numbers)))
+print("The largest number is {}".format(max(numbers)))
+print("The average of the numbers is {}".format(sum(numbers)/len(numbers)))
+
+
+usernames = ['jimbo', 'giltson98', 'derekf', 'WhatSup', 'NicolEye', 'swei45', 'BaseInterpreterInterface', 'BaseStdIn', 'Command', 'ExecState', 'InteractiveConsole', 'InterpreterInterface', 'StartServer', 'bob']
+
+str_user_name = input("Please enter your user name: ")
+if str_user_name in usernames:
+    print("Access granted")
+else:
+    print("Access denied")
