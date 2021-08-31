51be59dda9af11a56418d070ee4cc9a355355e8d
Mike <michael.robinson1@my.jcu.edu.au>
2021-08-31 13:53:00 +1000

    Refactor subject_reader.py to fully use functions

--- cp1404practicals/prac_04/subject_reader.py
+++ cp1404practicals/prac_04/subject_reader.py
@@ -8,22 +8,29 @@ FILENAME = "subject_data.txt"
 
 def main():
     data = get_data()
-    print(data)
+    print_subject_details(data)
+
+
+def print_subject_details(data):
+    for element in data:
+        print("{} is taught by {:12s} and has {:3} students".format(element[0], element[1], element[2]))
 
 
 def get_data():
     """Read data from file formatted like: subject,lecturer,number of students."""
     input_file = open(FILENAME)
+    lst_subjects = []
     for line in input_file:
-        print(line)  # See what a line looks like
-        print(repr(line))  # See what a line really looks like
+        # print(line)  # See what a line looks like
+        # print(repr(line))  # See what a line really looks like
         line = line.strip()  # Remove the \n
         parts = line.split(',')  # Separate the data into its parts
-        print(parts)  # See what the parts look like (notice the integer is a string)
+        # print(parts)  # See what the parts look like (notice the integer is a string)
         parts[2] = int(parts[2])  # Make the number an integer (ignore PyCharm's warning)
-        print(parts)  # See if that worked
-        print("----------")
+        # print(parts)  # See if that worked
+        # print("----------")
+        lst_subjects.append(parts)
     input_file.close()
-
+    return lst_subjects
 
 main()
